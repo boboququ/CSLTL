ab70608211c24d671d5cc77e3f0dd01f5a6cbf98
Kevin Li <16keli@gmail.com>
2019-02-03 15:45:33 -0500

    Cleanup formatting and strings, and add 5 most played heroes in past 100 games (#2)
    * Format code and add gitignore. PEP8 compliance coming soon
    
    * Cleanup formatting a bit and add 5 most recently played heroes in last 100 games

--- slack_bot.py
+++ slack_bot.py
@@ -1,23 +1,26 @@
-from slackclient import SlackClient
-from teamlookup import *
+"""Slack bindings for TangyBot (rip)."""
 import os
 import time
+
+from slackclient import SlackClient
+
+from teamlookup import *
+
 
 class CSL_Lookup_Bot(object):
     def __init__(self, bot_token):
         self.slack_client = SlackClient(bot_token)
         self.app_name = "tangy_bot"
 
-    def slackConnect(self):
+    def slack_connect(self):
         success = self.slack_client.rtm_connect()
         print("Connection status:", success)
         return
 
-    def slackreadRTM(self):
+    def slack_read_rtm(self):
         return self.slack_client.rtm_read()
 
-
-    def Parse_Slack_Input(self, input, botID):
+    def parse_slack_input(self, input, botID):
         if input == []:
             return None
         bot_at_ID = "<@" + botID + ">"
@@ -29,39 +32,43 @@ class CSL_Lookup_Bot(object):
                     user = input['user']
                     message = input['text'].split(bot_at_ID)[1].strip(" ")
                     channel = input["channel"]
-                    print("user:", user, "Message:", message,"channel:", channel)
-                    print("MESSAGE WAS GOT:", str(message), "ON ", str(channel))
+                    print("user:", user, "Message:", message, "channel:",
+                          channel)
+                    print("MESSAGE WAS GOT:", str(message), "ON ",
+                          str(channel))
                     return [str(user), str(message), str(channel)]
         else:
             return None
 
-    def Get_Bot_ID(self, botname):
-        #call slack api
+    def get_bot_id(self, botname):
+        # call slack api
         api_call = self.slack_client.api_call("users.list")
         users = api_call["members"]
         for user in users:
-            if "name" in user and botname in user.get('name') and not user.get("deleted"):
+            if "name" in user and botname in user.get('name') and not user.get(
+                    "deleted"):
                 return user.get("id")
-        
-    def Write_To_Slack(self, channel, message):
+
+    def write_to_slack(self, channel, message):
         print("Writing to slack")
-        return self.slack_client.api_call("chat.postMessage", channel = channel, text = message, as_user = True)
-    
-    def Decide_To_Take_Action(self, message):
+        return self.slack_client.api_call("chat.postMessage", channel=channel,
+                                          text=message, as_user=True)
+
+    def decide_to_take_action(self, message):
         print("Deciding Whether To Take Action")
         message = message.lower()
         if "help" in message:
             return 1
         if "lookup" in message:
             return 2
-        
-        return 0    
-    
-    def Handle_Help_Message(self, channel):
+
+        return 0
+
+    def handle_help_message(self, channel):
         message = "How to Use: @tangy_bot lookup<space><team_url or team id> \n"
-        self.Write_To_Slack(channel, message)
-    
-    def Handle_Lookup_Message(self, message, channel):
+        self.write_to_slack(channel, message)
+
+    def handle_lookup_message(self, message, channel):
         message = message.split(" ")
         team_id = message[1]
         team_id = team_id.strip(">")
@@ -70,36 +77,36 @@ class CSL_Lookup_Bot(object):
         print(team_id)
         return_message = look_up_team(team_id)
         if return_message == "":
-            return_message = "404 error" 
-        self.Write_To_Slack(channel, return_message)
+            return_message = "404 error"
+        self.write_to_slack(channel, return_message)
 
     def run(self):
-        self.slackConnect()
+        self.slack_connect()
         my_name = self.app_name
-        my_id = self.Get_Bot_ID(my_name)
+        my_id = self.get_bot_id(my_name)
         print("My id is", my_id)
         while True:
-            result = self.Parse_Slack_Input(self.slackreadRTM(), my_id)
-            
+            result = self.parse_slack_input(self.slack_read_rtm(), my_id)
+
             if result is not None:
                 user = result[0]
                 message = result[1]
                 channel = result[2]
                 print(user, message, channel)
-                proceed = self.Decide_To_Take_Action(message)
+                proceed = self.decide_to_take_action(message)
                 if proceed > 0:
                     if proceed == 1:
-                        self.Handle_Help_Message(channel)
+                        self.handle_help_message(channel)
 
                     if proceed == 2:
-                        self.Handle_Lookup_Message(message, channel)
+                        self.handle_lookup_message(message, channel)
 
             time.sleep(1)
 
 
 if __name__ == "__main__":
-    #create instance of bot
-    #run bot
+    # create instance of bot
+    # run bot
     bot_token = os.environ.get("BOT_TOKEN")
     print(bot_token)
     bot_instance = CSL_Lookup_Bot(bot_token)
--- steamtest.py
+++ steamtest.py
@@ -1,28 +1,84 @@
-import requests
 import json
-individual_constant = 0x0110000100000000
+
+import requests
+
+import hero_data
+
+# Mapping of hero information based on hero id
+hero_info = hero_data.HeroData()
+# Magic number :O
+INDIVIDUAL_CONSTANT = 0x0110000100000000
+
 
 def convert_text_to_32id(steam_id):
-	steam_id = steam_id[6:]
-	#print(steam_id)
-	steam_id = steam_id.split(":")
-	instance = int(steam_id[1])
-	account_number = int(steam_id[2]) * 2 + instance + individual_constant
-	return account_number - 76561197960265728
+    steam_id = steam_id[6:]
+    # print(steam_id)
+    steam_id = steam_id.split(":")
+    instance = int(steam_id[1])
+    account_number = int(steam_id[2]) * 2 + instance + INDIVIDUAL_CONSTANT
+    return account_number - 76561197960265728
+
 
 def get_account_info(id_32):
-	api = "https://api.opendota.com/api/players/"
-	headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-	request = requests.get(api + str(id_32), headers = headers)
-	if request.status_code != 200:
-		print("bad call to api")
-	else:
-		JSON = json.loads(request.content.decode("utf-8"))
-		return JSON["solo_competitive_rank"], JSON["mmr_estimate"], JSON["rank_tier"], JSON["leaderboard_rank"]
+    api = "https://api.opendota.com/api/players/"
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    request = requests.get(api + str(id_32), headers=headers)
+    if request.status_code != 200:
+        print("bad call to api")
+    else:
+        JSON = json.loads(request.content.decode("utf-8"))
+        return JSON["solo_competitive_rank"], JSON["mmr_estimate"], JSON[
+            "rank_tier"], JSON["leaderboard_rank"]
+
+
+def get_account_heroes(id_32, matches_limit=100, num_heroes=5):
+    """
+    Get the most played heroes for this player.
+
+    For more information about the API endpoint used here, see the docs at:
+    https://docs.opendota.com/#tag/players%2Fpaths%2F~1players~1%7Baccount_id%7D~1heroes%2Fget
+
+    Additionally, we add the following key-value pairs to the dictionary:
+        loc_name, containing the hero's English name
+        winrate, containing the player's winrate with the hero
+
+    Parameters
+    ----------
+    id_32: int
+        The Steam32 ID of a player
+
+    matches_limit: int (default 100)
+        The number of recent matches to consider for this player.
+
+    num_heroes: int (default 5)
+        The number of most played heroes to display.
+
+    Returns
+    -------
+    played_heroes: list of dicts
+        The list of num_heroes heroes that the player has played the most.
+
+    """
+    api = "https://api.opendota.com/api/players/" + str(id_32) + "/heroes"
+    req = requests.get(api, params=dict(limit=matches_limit))
+    if req.status_code != 200:
+        print("bad call to api")
+    else:
+        ret = req.json()
+        for hero_dict in ret:
+            hero_dict['loc_name'] = hero_info[hero_dict['hero_id']]['loc_name']
+            try:
+                hero_dict['winrate'] = hero_dict['win'] / hero_dict['games']
+            except ZeroDivisionError:
+                hero_dict['winrate'] = 0.0
+        return sorted(ret,
+                      key=lambda item: item['games'],
+                      reverse=True)[:num_heroes]
+
 
 if __name__ == '__main__':
-	steam_id = "STEAM_0:1:51900704"
-	steam_32 = convert_text_to_32id(steam_id)
-	account_info = get_account_info(steam_32)
-	print(account_info)
-
+    steam_id = "STEAM_0:1:51900704"
+    steam_32 = convert_text_to_32id(steam_id)
+    account_info = get_account_info(steam_32)
+    print(account_info)
